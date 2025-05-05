0921ebaab9fc9e6cc1b4be85c99ad1b7878738a2
zzkat lin <3079791498@qq.com>
2025-05-05 15:56:41 +0800

    fix:fix type error

--- src/page/Day2.tsx
+++ src/page/Day2.tsx
@@ -1,6 +1,6 @@
-import p5 from "p5";
-import { defineComponent, onMounted, ref } from "vue";
-import { isDark } from "../composables/dark";
+import P5 from 'p5'
+import { defineComponent, onMounted, ref } from 'vue'
+import { isDark } from '../composables/dark'
 
 export default defineComponent({
   name: 'Day2',
@@ -9,43 +9,48 @@ export default defineComponent({
     const width = 500
     const height = width
 
-    onMounted(()=>{
-      new p5((p:p5)=>{
-        p.setup = ()=>{
-          p.createCanvas(width,height)
-        }
-        p.draw = ()=>{
-          p.background(isDark.value ? "#121212" : "#fff")
-          p.stroke(100)
-          p.noFill()
-          p.strokeWeight(width * 0.01)
-
-          const time = p.millis() / 1000
-          
-          const pinpong = p.sin(time * 0.75 - 0.5 * p.PI) * 0.5 + 0.5 // 创建一个从0到1的值
-          
-          const points = p.lerp(2,100,p.pow(pinpong,2.5)) // 使用 ping + pow 去生成一个非线形的生成过程
-          
-          const radius = width / 2.1
-          const angle = pinpong * p.PI * 2;
-          polygon(width/2,height/2,radius,points,angle)
-        }
-
-        function polygon(x:number,y:number,radius:number,points = 3,angle = 0){
-          p.beginShape()
-          for(let i = 0; i < points;i++){
-            const a = p.TWO_PI * i / points
-            const lx = x + p.cos(a + angle) * radius
-            const ly = y + p.sin(a + angle) * radius
-            p.vertex(lx,ly)
+    onMounted(() => {
+      if (canvasRef.value) {
+        const sketch = new P5((p: P5) => {
+          p.setup = () => {
+            p.createCanvas(width, height)
           }
-          p.endShape(p.CLOSE)
-        }
-        
-      },canvasRef.value)
+          p.draw = () => {
+            p.background(isDark.value ? '#121212' : '#fff')
+            p.stroke(100)
+            p.noFill()
+            p.strokeWeight(width * 0.01)
+
+            const time = p.millis() / 1000
+
+            const pinpong = p.sin(time * 0.75 - 0.5 * p.PI) * 0.5 + 0.5 // 创建一个从0到1的值
+
+            const points = p.lerp(2, 100, p.pow(pinpong, 2.5)) // 使用 ping + pow 去生成一个非线形的生成过程
+
+            const radius = width / 2.1
+            const angle = pinpong * p.PI * 2
+            polygon(width / 2, height / 2, radius, points, angle)
+          }
+
+          function polygon(x: number, y: number, radius: number, points = 3, angle = 0) {
+            p.beginShape()
+            for (let i = 0; i < points; i++) {
+              const a = p.TWO_PI * i / points
+              const lx = x + p.cos(a + angle) * radius
+              const ly = y + p.sin(a + angle) * radius
+              p.vertex(lx, ly)
+            }
+            p.endShape(p.CLOSE)
+          }
+        }, canvasRef.value)
+
+        return () => sketch.remove()
+      }
     })
-    return () => <div class="flex-1 flex justify-center items-center " >
-      <div ref={canvasRef}></div>
-    </div>
-  }
+    return () => (
+      <div class="flex-1 flex justify-center items-center ">
+        <div ref={canvasRef}></div>
+      </div>
+    )
+  },
 })
